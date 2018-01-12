37a693d1a910c312ff31bb7b2c179e4a507bc3bb
Roxasbtg <34135805+Roxasbtg@users.noreply.github.com>
2018-01-12 11:44:16 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-document.body.appendChild(document.createTextNode(" and JaP is kewl"));
+// document.body.appendChild(document.createTextNode(" and JaP is kewl"));
+
+// TPYJKEJ JASH CODEWD
+document.getElementById("demo").innerHTML = "Roxasbtg is kewl";
+
+var lb = document.getElementById("lb"),
+    src = {
+      on: "https://www.w3schools.com/js/pic_bulbon.gif",
+      off: "https://www.w3schools.com/js/pic_bulboff.gif"
+    };
+
+document.getElementById("demo").onclick = function() {
+  lb.src = src.on;
+  document.getElementById("demo").onclick=function(){
+    lb.src = src.off;
+  };
+};
