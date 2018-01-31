a7f2fad1d7167d8adfdf9f673393cb5c2fbe1592
JaPNaA <leonehuang100@gmail.com>
2018-01-31 13:03:24 -0500

    Add table of contents, darken rainbow
    Rainbow darkened by 50lum -> 25lum
    Table of contents works by inserting <a name>s before <h1>s and linking to them.

--- script.js
+++ script.js
@@ -1,5 +1,28 @@
 // document.body.appendChild(document.createTextNode(" and JaP is kewl"));
 
+(function() {
+    var e = document.getElementById("tableOfContents"),
+        x = document.getElementsByTagName("h1"),
+        ix = 0;
+        
+    for(let i of x) {
+        let n = document.createElement("li"),
+            m = document.createElement("a");
+            
+        m.name = ++ix;
+        i.parentElement.insertBefore(m, i);
+        
+        {
+            let c = document.createElement("a");
+            c.href = "#" + ix;
+            c.innerHTML = i.innerHTML;
+            n.appendChild(c);
+        }
+        
+        e.appendChild(n);
+    }
+}());
+
 // TPYJKEJ JASH CODEWD
 document.getElementById("demo").innerHTML = "Roxasbtg is kewl";
 
