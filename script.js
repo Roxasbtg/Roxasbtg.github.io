b1d5ac24b2564656c1366a2328d739dfab9e0c05
JaPNaA <leonehuang100@gmail.com>
2018-01-31 21:43:47 -0500

    prettify
    because coding on a raspberry pi doesn't prettify code for you.

--- script.js
+++ script.js
@@ -1,24 +1,24 @@
 // document.body.appendChild(document.createTextNode(" and JaP is kewl"));
 
-(function() {
+(function () {
     var e = document.getElementById("tableOfContents"),
         x = document.getElementsByTagName("h1"),
         ix = 0;
-        
-    for(let i of x) {
+
+    for (let i of x) {
         let n = document.createElement("li"),
             m = document.createElement("a");
-            
+
         m.name = ++ix;
         i.parentElement.insertBefore(m, i);
-        
+
         {
             let c = document.createElement("a");
             c.href = "#" + ix;
             c.innerHTML = i.innerHTML;
             n.appendChild(c);
         }
-        
+
         e.appendChild(n);
     }
 }());
